ecb485f86a07918f2f3085bea57f3cf61287e2e9
magikalex81 <magikalex81@gmail.com>
2014-07-07 16:25:20 +0200

    Create getserver.php

---
+++ alex1.acticia.net/php/getserver.php
@@ -0,0 +1,8 @@
+<?php
+	$result = array(
+		0 => array("url" => "alex1.acticia.net", "port" => 80),
+		1 => array("url" => "alex2.acticia.net", "port" => 80)
+	);
+		
+	echo json_encode($result);
+?>
