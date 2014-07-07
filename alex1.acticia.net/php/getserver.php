75f6be826c39f8629d510b7e48599b46f2e0087c
magikalex81 <magikalex81@gmail.com>
2014-07-07 22:33:30 +0200

    Update getserver.php

--- alex1.acticia.net/php/getserver.php
+++ alex1.acticia.net/php/getserver.php
@@ -1,7 +1,7 @@
 <?php
 	$result = array(
-		0 => array("url" => "alex1.acticia.net", "port" => 80),
-		1 => array("url" => "alex2.acticia.net", "port" => 80)
+		0 => array("host" => "alex1.acticia.net", "uploadUrl" => "/php/upload.php"), //le port peut etre spécifié dans l'url ex: alex1.acticia.net:8080
+		1 => array("host" => "alex2.acticia.net", "uploadUrl" => "/php/upload.php")
 	);
 		
 	echo json_encode($result);
