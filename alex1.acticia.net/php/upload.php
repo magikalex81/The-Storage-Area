6a2274e28e6062bb69e3e77986bdaad9a00dbb61
magikalex81 <magikalex81@gmail.com>
2014-07-07 16:24:16 +0200

    Update upload.php

--- alex1.acticia.net/php/upload.php
+++ alex1.acticia.net/php/upload.php
@@ -1,14 +1,59 @@
 <?php
-$GLOBALS['destFolder'] = 'files';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST'); 
+
+$GLOBALS['destFolder'] = '../files';
 
 error_reporting(-1);
 
-if (isset($_FILES['myfile'])) {
-	$sFileName = $_FILES['myfile']['name'];
-	$sFileType = $_FILES['myfile']['type'];
-	$sFileSize = bytesToSize1024($_FILES['myfile']['size']);
-	move_uploaded_file($_FILES['myfile']['tmp_name'], $GLOBALS['destFolder'].'/'.$sFileName);
-} else {
-	echo 'An error occurred';
+
+/**
+ *
+ * Logging operation - to a file (upload_log.txt) and to the stdout
+ * @param string $str - the logging string
+ */
+function _log($str) {
+
+    // log to the output
+    $log_str = date('d.m.Y').": {$str}\r\n";
+    echo $log_str;
+
+    // log to file
+    if (($fp = fopen('upload_log.txt', 'a+')) !== false) {
+        fputs($fp, $log_str);
+        fclose($fp);
+    }
 }
+
+
+
+// loop through files and move the chunks to a temporarily created directory
+if (!empty($_FILES)) foreach ($_FILES as $file) {
+
+    // check the error status
+    if ($file['error'] != 0) {
+        _log('error '.$file['error'].' in file '.$_POST['fileName']);
+        continue;
+    }
+
+    // init the destination file (format <filename.ext>.part<#chunk>
+    // the file is stored in a temporary directory
+    $temp_dir = $GLOBALS['destFolder'];
+    if ($_POST['totalChunks'] != 1) $temp_dir .= '/'.$_POST['fileName'];
+    
+    $dest_file = $temp_dir.'/'.$_POST['fileName'];
+	if ($_POST['totalChunks'] != 1) $dest_file .= '.part'.$_POST['chunkId'];
+	
+    // create the temporary directory
+    if (!is_dir($temp_dir)) {
+        mkdir($temp_dir, 0777, true);
+    }
+
+    // move the temporary file
+    if (!move_uploaded_file($file['tmp_name'], $dest_file)) {
+        _log('Error saving (move_uploaded_file) chunk '.$_POST['chunkId'].' for file '.$_POST['fileName']);
+    }
+}
+
+
 ?>
