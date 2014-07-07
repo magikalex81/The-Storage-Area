0aa08a20494c3a20ba2e8fefeaaf318b1c9bf528
magikalex81 <magikalex81@gmail.com>
2014-07-07 16:23:36 +0200

    Update index.php

--- alex1.acticia.net/index.php
+++ alex1.acticia.net/index.php
@@ -1,12 +1,43 @@
+<?php
+function bytesToSize1024($bytes) {
+    $unit = array('B','KiB','MiB','GiB');
+    return @round($bytes / pow(1024, ($i = floor(log($bytes, 1024)))), 1).' '.$unit[$i];
+}
+$df = bytesToSize1024(disk_free_space("/"));
+?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>alex1 modifie</title>
-  </head>
-  <body>
-    ALEX1.ACTICIA.NET:80 VHOST ON TRESCA.ACTICIA.NET VPS ON VPS71303.OVH.NET
-    <a href="http://alex1.acticia.net/php/upload.source">upload.source</a>
-   <a href="http://alex1.acticia.net/files/touched.file">touched.file</a>
-  </body>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>(c) ACTICIA</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<header>
+			<h2>The Storage Area</h2>
+		</header>
+		<div class="container">
+			<div class="contr">
+				<h2>Upload your files (max 500 files at once)</h2>
+			</div>
+			<div class="upload_form_cont">
+				<div class="info">
+					<div id="dropArea">Click or Drop your files here</div>
+					<div>
+						<span id="count"></span>
+					</div>
+					<h3>Result:</h3>
+					<div id="result"></div>
+				</div>
+			</div>
+			<footer>
+				By <a href="http://www.acticia.com">ACTICIA</a><br>
+				This product does not comply with the right of Data Records Erasure provided by Member States in regard of the right to oblivion<br>
+				ACTICIA does not know how to erase data in this system<br>
+				Still <?=$df?> available on this node<br>
+				ALPHA RESTRICTIONS APPLIED</footer>
+		</div>
+		<script src="js/file_uploader.js"></script>
+		<script src="js/script.js"></script>
+	</body>
 </html>
