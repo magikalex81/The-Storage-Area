35b93872b3e30c3b388646f741a6dc31da1cd767
acticia <dev@acticia.com>
2014-07-09 13:31:13 +0000

    Create getentropy.js

---
+++ FileUploader/js/getentropy.js
@@ -0,0 +1,10 @@
+{
+    "jsonrpc": "2.0",
+    "method": "generateBlobs",
+    "params": {
+        "apiKey": "6b1e65b9-4186-45c2-8981-b77a9842c4f0",
+        "n": 1,
+        "size": 1024
+    },
+    "id": 42
+}
