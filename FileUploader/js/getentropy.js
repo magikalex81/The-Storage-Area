9c274a3d1474432bc55e2d23bb552af7162972ee
acticia <dev@acticia.com>
2014-07-09 14:38:38 +0000

    Update getentropy.js

--- FileUploader/js/getentropy.js
+++ FileUploader/js/getentropy.js
@@ -1,23 +1,21 @@
-'use strict';
+var url='https://api.random.org/json-rpc/1/invoke'
 
 var entropy256=({
-   'jsonrpc': '2.0',
-   'method': 'generateBlobs',
-   'params': {
-      'apiKey': '52679680-6404-4c86-af59-08a5b3fa5ca5',
-      'n': 1,
-      'size': 256,
-      'format': 'hex',
-      },
-   'id': 42,
+   "jsonrpc": "2.0",
+   "method": "generateBlobs",
+   "params": {
+      "apiKey": "52679680-6404-4c86-af59-08a5b3fa5ca5",
+      "n": 1,
+      "size": 256,
+      "format": "hex"
+   },
+   "id": 42
 });
 
-return{
-   return $http({
-      url: 'https://api.random.org/json-rpc/1/invoke',
-      method: 'POST',
-      headers:{'Content-Type':'application/json-rpc'},
-      data: entropy256
-      });
-}
-console.debug(response)
+
+
+
+
+
+
+console.debug(LA_CHOSE)
