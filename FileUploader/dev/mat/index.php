7d2063146b54693273a300942921d1b7ce120279
acticia <dev@acticia.com>
2014-07-11 09:30:53 +0000

    Update index.php

--- FileUploader/dev/mat/index.php
+++ FileUploader/dev/mat/index.php
@@ -1,1 +1,16 @@
-<>
+<?php
+//
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>(c) ACTICIA</title>
+		<link href="../../css/main.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<header>
+    <script type="text/javascript" src="../../js/sha256mod.js"></script>
+	  <script type="text/javascript" src="../../js/getentropy.js"></script>
+	</body>
+</html>
