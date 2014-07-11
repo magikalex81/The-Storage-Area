4757583a220d0b582b959b853b921d2ccdb2dfff
acticia <dev@acticia.com>
2014-07-11 09:42:48 +0000

    Update index.php

--- FileUploader/dev/mat/index.php
+++ FileUploader/dev/mat/index.php
@@ -6,11 +6,13 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>(c) ACTICIA</title>
+		<!--
 		<link href="../../css/main.css" rel="stylesheet" type="text/css" />
+		-->
 		</head>
 	<body>
 		See the JS console<br>
-		<a href="http://validator.w3.org/check?uri=http%3A%2F%2Falex1.acticia.net%2Fdev%2Fmat">HTML5 VALIDATOR</a>
+		<a href="http://validator.w3.org/check?uri=http%3A%2F%2Falex1.acticia.net%2Fdev%2Fmat" target="1">HTML5 VALIDATOR</a>
 		<script type="text/javascript" src="../../js/sha256mod.js"></script>
 		<script type="text/javascript" src="../../js/getentropy.js"></script>
 	</body>
