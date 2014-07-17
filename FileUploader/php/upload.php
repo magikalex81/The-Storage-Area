7b6b327846be831d41c585f1fa7ce56e7835cedf
acticia <dev@acticia.com>
2014-07-17 08:39:16 +0000

    Update upload.php

--- FileUploader/php/upload.php
+++ FileUploader/php/upload.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST'); 
+//header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Methods: POST'); 
 
 $GLOBALS['destFolder'] = '../files';
 
