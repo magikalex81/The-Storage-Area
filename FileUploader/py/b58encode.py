b5fa5b2c8c09770d40a223c417209b7764774cb8
acticia <dev@acticia.com>
2014-07-21 14:37:20 +0000

    Create b58encode.py

---
+++ FileUploader/py/b58encode.py
@@ -0,0 +1,24 @@
+## (c) 2014 by Matthieu VIGUIER. CC-BY-SA 4.0 INTERNATIONAL
+
+alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+base_count = len(alphabet)
+       
+def b58encode(num):
+    ## num is a integer
+    encode = ''
+   
+    if (num < 0):
+        return ''
+   
+    while (num >= base_count):    
+        mod = num % base_count
+        encode = alphabet[mod] + encode
+        num = num // base_count
+ 
+    if (num):
+        encode = alphabet[num] + encode
+ 
+    return encode
+
+## TEST
+assert b58encode(4000000000)  == '76U3eF', 'Issue found !'
