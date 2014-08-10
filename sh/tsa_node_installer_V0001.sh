68a45818a48d48b034f70063d440baf20468e72d
acticia <admin@acticia.com>
2014-08-11 00:30:23 +0200

    Update tsa_node_installer_V0001.sh
    anti rootkit, logchecker, mail server,

--- sh/tsa_node_installer_V0001.sh
+++ sh/tsa_node_installer_V0001.sh
@@ -1,3 +1,6 @@
+DO NOT RUN IT IS A MEMO
+
+
 #!/bin/bash
 # wget --no-check-certificate https://raw.githubusercontent.com/magikalex81/The-Storage-Area/master/sh/tsa_node_installer_V0001.sh
 # THIS IS THE TSA INSTALLER V0001
@@ -27,7 +30,7 @@ read upass
 /usr/sbin/dpkg-reconfigure openssh-server
 /bin/sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
-/usr/bin/apt-get install -y git exim4 cron-apt chkrootkit rkhunter tripwire clamav
+## ZUI ## /usr/bin/apt-get install -y git exim4 cron-apt chkrootkit rkhunter tripwire clamav
 # CONF MAIL
 /bin/sed -i "s/dc_eximconfig_configtype='local'/dc_eximconfig_configtype='internet'/" /etc/exim4/update-exim4.conf.conf
 /bin/sed -i "s/dc_local_interfaces='127.0.0.1 ; ::1'/dc_local_interfaces='<; ::0 ; 127.0.0.1''/" /etc/exim4/update-exim4.conf.conf ##ZUI IPV4 LOCKED
@@ -39,6 +42,12 @@ read email
 # AUTO UPDATE
 /bin/echo MAILON="output" >> /etc/cron-apt/config
 /bin/echo DEBUG="verbose" >> /etc/cron-apt/config
+# ANTI-ROOTKIT
+(/usr/sbin/chkrootkit 2>&1 | mail -s "chkrootkit output" root)
+rkhunter --update
+rkhunter --propupd
+/bin/sed -i 's/MAIL-ON-WARNING=""/MAIL-ON-WARNING="root"/' /etc/etc/rkhunter.conf
+(/usr/bin/rkhunter -c --enable all --disable none --rwo --cronjob --update 2>&1 | mail -s "rkhunter warnings" root)
 # SEND LOGS
 /bin/tar -czvf install_log.tar.gz -C / var/log
 /usr/bin/mail -s "$HOSTNAME LOG" -a /root/install_log.tar.gz root < /dev/null
