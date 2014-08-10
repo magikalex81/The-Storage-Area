136720966ebbc3b62f649fb0497d85db5842e647
acticia <admin@acticia.com>
2014-08-11 00:07:22 +0200

    Update tsa_node_installer_V0001.sh

--- sh/tsa_node_installer_V0001.sh
+++ sh/tsa_node_installer_V0001.sh
@@ -2,8 +2,6 @@
 # wget --no-check-certificate https://raw.githubusercontent.com/magikalex81/The-Storage-Area/master/sh/tsa_node_installer_V0001.sh
 # THIS IS THE TSA INSTALLER V0001
 # CHANGE THE DEFAULT ROOT PASSWORD
-clear
-/usr/bin/dpkg --configure -a
 /bin/echo -e "\e[1;32mHello, "$USER".  This step will ask you for a new password for root. Be sure to type on a secured keyboard with secured eyes because this password will not be confirmed and will be showed in clear !\e[0;m"
 /bin/echo -ne "\e[1;32mEnter your new password and press [ENTER]:\e[0;m "
 read rpass
@@ -16,12 +14,6 @@ read ulogin
 read upass
 /usr/sbin/useradd $ulogin
 /bin/echo -e "$ulogin:$upass" | /usr/sbin/chpasswd
-# RESTRICT ROOT FROM LOGIN DIRECTLY VIA SSH
-/bin/echo -e "\e[1;32mPlease wait ...\e[0;m"
-##/bin/rm /etc/ssh/ssh_host_*
-## /usr/sbin/dpkg-reconfigure openssh-server
-/bin/sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-/etc/init.d/ssh restart
 # RENEW SOURCE LIST
 /bin/echo deb http://http.debian.net/debian wheezy main contrib non-free > /etc/apt/sources.list
 /bin/echo deb http://http.debian.net/debian wheezy-updates main contrib non-free >> /etc/apt/sources.list
@@ -29,10 +21,16 @@ read upass
 /usr/bin/apt-get update -y
 /usr/bin/apt-get dist-upgrade -y
 /usr/bin/apt-get autoremove --purge -y
-/usr/bin/apt-get install -y git rsyslog exim4 cron-apt rkhunter clamav apache2 php5
+# RESTRICT ROOT FROM LOGIN DIRECTLY VIA SSH
+/bin/echo -e "\e[1;32mPlease wait ...\e[0;m"
+/bin/rm /etc/ssh/ssh_host_*
+/usr/sbin/dpkg-reconfigure openssh-server
+/bin/sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+/usr/bin/apt-get install -y git exim4 cron-apt chkrootkit rkhunter tripwire clamav
 # CONF MAIL
 /bin/sed -i "s/dc_eximconfig_configtype='local'/dc_eximconfig_configtype='internet'/" /etc/exim4/update-exim4.conf.conf
-/bin/sed -i "s/dc_local_interfaces='127.0.0.1 ; ::1'/dc_local_interfaces='127.0.0.1'/" /etc/exim4/update-exim4.conf.conf
+/bin/sed -i "s/dc_local_interfaces='127.0.0.1 ; ::1'/dc_local_interfaces='<; ::0 ; 127.0.0.1''/" /etc/exim4/update-exim4.conf.conf ##ZUI IPV4 LOCKED
 /bin/echo  -e"\e[1;32mThis step will ask you for email substitute for root.\e[0;m"
 /bin/echo -ne "\e[1;32mEnter your new email and press [ENTER]: \e[0;m"
 read email
