865f977a32694bdfcdf642bbe5d1e3fd19921e9b
acticia <dev@acticia.com>
2014-08-13 16:14:15 +0200

    Update mail_server.sh

--- sh/mail_server.sh
+++ sh/mail_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# wget --no-check-certificate https://raw.githubusercontent.com/magikalex81/The-Storage-Area/master/sh/mail_server.sh
+# wget --no-check-certificate https://raw.githubusercontent.com/magikalex81/The-Storage-Area/master/sh/mail_server.sh ; chmod u+x mail_server.sh ; ./mail_server.sh
 #
 # CHANGE THE DEFAULT ROOT PASSWORD
 /bin/echo -e "\e[1;32mHello, "$USER".  This step will ask you for a new password for root. Be sure to type on a secured keyboard with secured eyes because this password will not be confirmed and will be showed in clear !\e[0;m"
