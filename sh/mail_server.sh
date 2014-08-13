f5a29f274fe29135365d97a36170f4297e0e6bda
acticia <dev@acticia.com>
2014-08-13 16:22:02 +0200

    Update mail_server.sh

--- sh/mail_server.sh
+++ sh/mail_server.sh
@@ -2,8 +2,8 @@
 # wget --no-check-certificate https://raw.githubusercontent.com/magikalex81/The-Storage-Area/master/sh/mail_server.sh ; chmod u+x mail_server.sh ; ./mail_server.sh
 #
 # CHANGE THE DEFAULT ROOT PASSWORD
-/bin/echo -e "\e[1;32mHello, "$USER".  This step will ask you for a new password for root. Be sure to type on a secured keyboard with secured eyes because this password will not be confirmed and will be showed in clear !\e[0;m"
-/bin/echo -ne "\e[1;32mEnter your new password and press [ENTER]:\e[0;m "
+/bin/echo -e "Hello, "$USER".  This step will ask you for a new password for \e[1;32mroot\e[0;m. Be sure to type on a secured keyboard with secured eyes because this password will not be confirmed and will be showed in clear !"
+/bin/echo -ne "mEnter your new \e[1;32mpassword\e[0;m and press [ENTER]:\e[0;m "
 read rpass
 /bin/echo "root:$rpass" | /usr/sbin/chpasswd
 # ADD A NEW USER
@@ -16,6 +16,9 @@ read upass
 /bin/echo -e "$ulogin:$upass" | /usr/sbin/chpasswd
 ## PREPARING ENVIRONMENT
 /bin/echo -e "\e[1;32mPlease wait ...\e[0;m"
+/bin/mkdir /opt/acticia
+touch /opt/acticia/install.log
+touch /opt/acticia/install.err.log
 # RENEW SOURCE LIST
 /bin/echo deb http://http.debian.net/debian wheezy main contrib non-free > /etc/apt/sources.list
 /bin/echo deb http://http.debian.net/debian wheezy-updates main contrib non-free >> /etc/apt/sources.list
@@ -28,8 +31,11 @@ read upass
 /usr/sbin/dpkg-reconfigure openssh-server
 /bin/sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
-#/bin/echo -ne "\e[1;32mEnter your new $fqdn and press [ENTER]: \e[0;m"
-#read fqdn
+# PREPARE NETWORK
+/bin/echo -ne "Enter your new \e[1;32mfqdn\e[0;m and press [ENTER]:"
+read fqdn
+/bin/echo $fqdn > /etc/hostname
+127.0.0.1 localhost.localdomain localhost
 # AUTO UPDATE
 # ANTI-ROOTKIT
 # ANTI VIRUS
