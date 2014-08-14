167fbb72acdf267c468c5819f2a4d70bbe37f5b8
acticia <dev@acticia.com>
2014-08-14 14:20:05 +0200

    Update mail_server.sh

--- sh/mail_server.sh
+++ sh/mail_server.sh
@@ -2,23 +2,20 @@
 # wget --no-check-certificate https://raw.githubusercontent.com/magikalex81/The-Storage-Area/master/sh/mail_server.sh ; chmod u+x mail_server.sh ; ./mail_server.sh
 #
 # CHANGE THE DEFAULT ROOT PASSWORD
-### /bin/echo -e "Hello, "$USER".  This step will ask you for a new password for \e[1;32mroot\e[0;m. Be sure to type on a secured keyboard with secured eyes because this password will not be confirmed and will be showed in clear !"
-### /bin/echo -ne "Enter your new \e[1;32mpassword\e[0;m and press [ENTER]:\e[0;m "
-### read rpass
-rpass = 'toor'
+/bin/echo -e "Hello, "$USER".  This step will ask you for a new password for \e[1;32mroot\e[0;m. Be sure to type on a secured keyboard with secured eyes because this password will not be confirmed and will be showed in clear !"
+/bin/echo -ne "Enter your new \e[1;32mpassword\e[0;m and press [ENTER]:\e[0;m "
+read rpass
 /bin/echo "root:$rpass" | /usr/sbin/chpasswd
 # ADD A NEW USER
-### /bin/echo -e "This step will ask you for a \e[1;32mlogin and a password\e[0;m for a new user."
-### /bin/echo -ne "Enter your new \e[1;32mlogin\e[0;m and press [ENTER]: "
-### read ulogin
-### /bin/echo -ne "Enter your new \e[1;32mpassword\e[0;m for $ulogin and press [ENTER]: "
-### read upass
-ulogin = 'user'
-upass = 'resu'
+/bin/echo -e "This step will ask you for a \e[1;32mlogin and a password\e[0;m for a new user."
+/bin/echo -ne "Enter your new \e[1;32mlogin\e[0;m and press [ENTER]: "
+read ulogin
+/bin/echo -ne "Enter your new \e[1;32mpassword\e[0;m for $ulogin and press [ENTER]: "
+read upass
 /usr/sbin/useradd $ulogin
 /bin/echo -e "$ulogin:$upass" | /usr/sbin/chpasswd
 ## PREPARING ENVIRONMENT
-### /bin/echo -e "\e[1;32mPlease wait ...\e[0;m"
+/bin/echo -e "\e[1;32mPlease wait ...\e[0;m"
 /bin/mkdir /opt/acticia
 touch /opt/acticia/install.log
 touch /opt/acticia/install.err.log
@@ -48,9 +45,8 @@ read fqdn
 # PREPARE SYSTEM
 DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get install -y bzip2 gcc libpcre3-dev libpcre++-dev g++ libtool libmysqlclient-dev make libssl-dev libmysqld-dev libdb-dev automake autoconf bzip2 libzip2 libbz2-1.0 libbz2-dev curl libcurl3 libcurl4-openssl-dev libexpat1 libexpat1-dev libapache2-mod-php5 php5-mysql postfix-mysql postfix-pcre mysql-client-5.5 mysql-server-5.5 libsasl2-2 libsasl2-modules sasl2-bin openssl ntp
 /bin/mkdir /etc/caremail
-### /bin/echo -ne "Enter your new \e[1;32mroot password for SQL\e[0;m and press [ENTER]:"
-### read sqlroot
-sqlroot = 'sqltoor'
+/bin/echo -ne "Enter your new \e[1;32mroot password for SQL\e[0;m and press [ENTER]:"
+read sqlroot
 mysqladmin -u root password $sqlroot
 mysqladmin -u root --password=$sqlroot create postfix
 ## ZUI SQL PASSWORD IS HARDCODED FOR NOW
